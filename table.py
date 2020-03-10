8924b72b51876834200558cd76ba88f12e13efe9
YuliyaKarpenko <59172495+YuliyaKarpenko@users.noreply.github.com>
2020-03-10 12:46:31 +0300

    Create table.py

---
+++ table.py
@@ -0,0 +1,12 @@
+element_number = int(input ('Type in element's number:'))
+  if element_number == 3:
+    print('The element is  '+ 'Li')
+  elif element_number == 25:
+    print('The element is  '+ 'Mn')
+  elif element_number == 80:
+    print('The element is  '+ 'Hg')
+  elif element_number == 17:
+    print('The element is  '+ 'Cl')
+    
+  else:
+    print('No data')
